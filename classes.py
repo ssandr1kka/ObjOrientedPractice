ff366ce98609219ad0c2dd04474304d72e97df5a
ssandr1kka <ssandr1kka@yahoo.com>
2020-10-12 19:31:39 +0300

    First draft

---
+++ classes.py
@@ -0,0 +1,20 @@
+class Flight:
+    def __init__(self, origin, destination, duration):
+        self.origin = origin
+        self.destination = destination
+        self.duration = duration
+    def print_info(self):
+        print(f"Flight origin: {self.origin}")
+        print(f"Flight destination: {self.destination}")
+        print(f"Flight duration: {self.duration}")
+
+
+def main():
+    f1 = Flight(origin="New York", destination="Moscow", duration="950")
+    f1.print_info()
+
+    f2 = Flight(origin="Tbilisi", destination="Nafplio", duration="450")
+    f2.print_info()
+
+if __name__ == "__main__":
+    main()
