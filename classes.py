037eb87fe415ec06cd3c4436d7f16612314cd271
ssandr1kka <ssandr1kka@yahoo.com>
2020-10-12 20:49:01 +0300

    Added passengers

--- classes.py
+++ classes.py
@@ -1,5 +1,10 @@
 class Flight:
+    counter = 1
+
     def __init__(self, origin, destination, duration):
+        self.id = Flight.counter
+        Flight.counter += 1
+        self.passengers = []
         self.origin = origin
         self.destination = destination
         self.duration = duration
@@ -9,18 +14,34 @@ class Flight:
         print(f"Flight destination: {self.destination}")
         print(f"Flight duration: {self.duration}")
 
+        print()
+        print("Passengers:")
+        for passenger in self.passengers:
+            print(f"{passenger.name}")
+
     def delay(self, amount):
         self.duration += amount
+
+    def add_passenger(self, p):
+        self.passengers.append(p)
+        p.flight_id = self.id
+
+
+class Passenger:
+
+    def __init__(self, name):
+        self.name = name
 
 
 def main():
     f1 = Flight(origin="New York", destination="Moscow", duration=950)
-    f1.delay(50)
+
+    alice = Passenger(name="Alice")
+    bob = Passenger(name="Bob")
+    f1.add_passenger(alice)
+    f1.add_passenger(bob)
+
     f1.print_info()
-
-    f2 = Flight(origin="Tbilisi", destination="Nafplio", duration=450)
-    f2.print_info()
-
 
 if __name__ == "__main__":
     main()
